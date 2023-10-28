2f1efb25a391e86e97ee66fda548db279b81eb88
BiancaScoro <120531347+BiancaScoro@users.noreply.github.com>
2023-10-27 22:17:07 -0400

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,27 @@
+let shopItemsData = [
+    {
+    id: "gsfdgfd",
+    name: "Long Sleeve Shirt",
+    price: 30,
+    desc: "amus sed faucibus sapien. Cras iaculis ullamcorper nisi at condimen.",
+    img: "images/longsleeve.png"
+}, {
+    id: "gnjrkndj",
+    name: "Button Up Shirt",
+    price: 50,
+    desc: "amus sed faucibus sapien. Cras iaculis ullamcorper nisi at condimen.",
+    img: "images/buttonup.jpg"
+}, {
+    id: "gnrfkdn",
+    name: "Brown Jacket",
+    price: 40,
+    desc: "amus sed faucibus sapien. Cras iaculis ullamcorper nisi at condimen.",
+    img: "images/brownjacket.png"
+}, {
+    id: "gnjkdf",
+    name: "Short Sleeve Shirt",
+    price: 10,
+    desc: "amus sed faucibus sapien. Cras iaculis ullamcorper nisi at condimen.",
+    img: "images/t-shirt.jpg"
+},
+];
